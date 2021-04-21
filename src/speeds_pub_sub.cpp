f658d9c24d3ae60a2815e70a15b8cd4b3044a2b1
DavideFoini <79703903+DavideFoini@users.noreply.github.com>
2021-04-21 23:47:53 +0200

    Completed synchronizer

--- src/speeds_pub_sub.cpp
+++ src/speeds_pub_sub.cpp
@@ -5,51 +5,48 @@
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include "std_msgs/Float64.h"
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
 
 class pub_sub
 {
-
-
-  public:
+  private:
   ros::NodeHandle n;
-  chicago::motor_speeds speeds;
   ros::Publisher pub;
-  message_filters::Subscriber<chicago::MotorSpeed> fl;
-  message_filters::Subscriber<chicago::MotorSpeed> rr;
-  message_filters::Subscriber<chicago::MotorSpeed> rl;
-    
-  public:
-    pub_sub(){
-      message_filters::Subscriber<chicago::MotorSpeed> fr(n, "/motor_speed_fl", 1);
-      message_filters::Subscriber<chicago::MotorSpeed> fl(n, "/motor_speed_fl", 1);
-      message_filters::Subscriber<chicago::MotorSpeed> rr(n, "/motor_speed_rr", 1);
-      message_filters::Subscriber<chicago::MotorSpeed> rl(n, "/motor_speed_rl", 1);    
-      pub = n.advertise<chicago::motor_speeds>("/motor_speeds", 1);
-      message_filters::TimeSynchronizer<chicago::MotorSpeed, 
-                                      chicago::MotorSpeed,
-                                      chicago::MotorSpeed,
-                                      chicago::MotorSpeed> sync(fr, fl, rr, rl, 10);
 
-      sync.registerCallback(boost::bind(callback, _1, _2, _3, _4));
+  public:
+  pub_sub(){
+    pub = n.advertise<chicago::motor_speeds>("/speeds", 1000);
+    message_filters::Subscriber<chicago::MotorSpeed> fr(n, "/motor_speed_fr", 1);
+    message_filters::Subscriber<chicago::MotorSpeed> fl(n, "/motor_speed_fl", 1);
+    message_filters::Subscriber<chicago::MotorSpeed> rr(n, "/motor_speed_rr", 1);
+    message_filters::Subscriber<chicago::MotorSpeed> rl(n, "/motor_speed_rl", 1);
+
+    // policy
+    typedef message_filters::sync_policies
+    ::ApproximateTime<chicago::MotorSpeed, chicago::MotorSpeed, chicago::MotorSpeed,chicago::MotorSpeed> MySyncPolicy;
+
+    message_filters::Synchronizer<MySyncPolicy> sync(MySyncPolicy(10), fr, fl, rr, rl);
+
+    sync.registerCallback(boost::bind(&pub_sub::callback, this, _1, _2, _3, _4));
+    ros::spin();
   }
 
-  void callback(chicago::MotorSpeed& fr, 
-              chicago::MotorSpeed& fl,
-              chicago::MotorSpeed& rr,
-              chicago::MotorSpeed& rl) {
+  void callback(const chicago::MotorSpeed::ConstPtr& fr, const chicago::MotorSpeed::ConstPtr& fl, 
+                const chicago::MotorSpeed::ConstPtr& rr, const chicago::MotorSpeed::ConstPtr& rl) {
+      chicago::motor_speeds speeds;
       speeds.header.stamp = ros::Time::now();
       speeds.header.frame_id = "";
-      speeds.rpm_fr=fr.rpm;
-      speeds.rpm_fl=fl.rpm;
-      speeds.rpm_rr=rr.rpm;
-      speeds.rpm_rl=rl.rpm;
+      speeds.rpm_fr=fr->rpm;
+      speeds.rpm_fl=fl->rpm;
+      speeds.rpm_rr=rr->rpm;
+      speeds.rpm_rl=rl->rpm;
       pub.publish(speeds);
   }
 };
 
 int main(int argc, char **argv)
 {
-  ros::NodeHandle n;
   ros::init(argc, argv, "speeds_pub_sub");
   pub_sub my_pub_sub;
   ros::spin();
