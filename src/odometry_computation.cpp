f07ec8da6a39c35238c1c019426120b9bb55065d
DavideFoini <79703903+DavideFoini@users.noreply.github.com>
2021-04-27 09:55:33 +0200

    Completed odometry computation with tf

--- src/odometry_computation.cpp
+++ src/odometry_computation.cpp
@@ -1,5 +1,5 @@
 #include "ros/ros.h"
-#include "chicago/odom.h"
+#include "chicago/custom_odom.h"
 #include "chicago/MotorSpeed.h"
 #include "chicago/motor_speeds.h"
 #include "geometry_msgs/Vector3Stamped.h"
@@ -11,21 +11,27 @@
 #include <math.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <tf/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 class pub_sub
 {
   private:
   ros::NodeHandle n;
-  ros::Publisher pub;
+  ros::Publisher pub_odom;
+  ros::Publisher pub_custom;
   ros::Subscriber sub;
-  chicago::odom odom;
+  chicago::custom_odom custom_odom;
+  nav_msgs::Odometry odom;
   ros::Time last_time;
   bool first;
+  tf2_ros::TransformBroadcaster br;
+  geometry_msgs::TransformStamped transformStamped;
 
   public:
   pub_sub(){
-    pub = n.advertise<chicago::odom>("/odometry", 1000);
+    pub_custom = n.advertise<chicago::custom_odom>("/custom_odometry", 1000);
+    pub_odom = n.advertise<nav_msgs::Odometry>("/odometry", 1000);
     sub = n.subscribe("/twist", 1000, &pub_sub::callback, this);
     last_time = ros::Time::now();
     first = true;
@@ -37,13 +43,13 @@ class pub_sub
       int method;
       std_msgs::String str_method;
       // get parameters
-      n.getParam("/position.x", x);
-      n.getParam("/position.y", y);
-      n.getParam("/position.th", th);
+      n.getParam("/x", x);
+      n.getParam("/y", y);
+      n.getParam("/th", th);
       n.getParam("/method", method);
-      odom.odom.header.stamp = ros::Time::now();
-      odom.odom.header.frame_id = "odom";
-      odom.odom.child_frame_id = "scout";
+      odom.header.stamp = ros::Time::now();
+      odom.header.frame_id = "odom";
+      odom.child_frame_id = "base_link";
 
       if(!first)
        dt = (twist->header.stamp - last_time).toSec();
@@ -66,19 +72,21 @@ class pub_sub
       }
 
       // updating parameters
-      n.setParam("/position.x", x);
-      n.setParam("/position.y", y);
-      n.setParam("/position.th", th);
+      n.setParam("/x", x);
+      n.setParam("/y", y);
+      n.setParam("/th", th);
 
       // set pose
-      odom.odom.pose.pose.position.x = x;
-      odom.odom.pose.pose.position.y = y;
-      odom.odom.pose.pose.position.z = 0;
-      geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(th);
-      odom.odom.pose.pose.orientation = odom_quat;
+      odom.pose.pose.position.x = x;
+      odom.pose.pose.position.y = y;
+      odom.pose.pose.position.z = 0;
+      tf2::Quaternion odom_quat;
+      odom_quat.setRPY(0, 0, th);
+      geometry_msgs::Quaternion quat_msg = tf2::toMsg(odom_quat);
+      odom.pose.pose.orientation = quat_msg;
 
       // set twist
-      odom.odom.twist.twist = twist->twist;
+      odom.twist.twist = twist->twist;
 
       //set method
       if(method == 0)
@@ -86,11 +94,27 @@ class pub_sub
       else
         str_method.data = "rk";
 
-      odom.method = str_method;
+      custom_odom.odom = odom;
+      custom_odom.method = str_method;
 
-      // TO DO tf transform
+      // publish messages
+      pub_custom.publish(custom_odom);
+      pub_odom.publish(odom);
 
-      pub.publish(odom);
+      // set TF
+      transformStamped.header.stamp = ros::Time::now();
+      transformStamped.header.frame_id = "world";
+      transformStamped.child_frame_id = "odom";
+      transformStamped.transform.translation.x = x;
+      transformStamped.transform.translation.y = y;
+      transformStamped.transform.translation.z = 0.0;
+      transformStamped.transform.rotation.x = odom_quat.x();
+      transformStamped.transform.rotation.y = odom_quat.y();
+      transformStamped.transform.rotation.z = odom_quat.z();
+      transformStamped.transform.rotation.w = odom_quat.w();
+
+      // send TF
+      br.sendTransform(transformStamped);
   }
 };
 
